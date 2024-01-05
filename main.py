2ca68d66834d861c4cc162dcdefa1d9916417b0d
kirk0830 <yike.huang@aliyun.com>
2024-01-05 18:43:27 +0800

    make old version also possible

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 """main"""
 
-def main(input_file: str):
+def main(input_file: str, version: str = "v1"):
     import apns.module_workflow.driver as amwd
-    amwd.driver_v1(input_file)
+    amwd.driver_v1(input_file) if version == "v1" else amwd.driver_v0(input_file)
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2:
-        main("input.json")
-    else:
+    if len(sys.argv) < 2: # interactive mode
+        main("input.json", "v1")
+    elif len(sys.argv) < 3: # command line mode
         main(sys.argv[1])
+    else: # command line mode with version
+        main(sys.argv[1], sys.argv[2])
