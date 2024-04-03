64b3ded15cb6c7aeedd3607cae13e65dafb6d7dc
kirk0830 <yike.huang@aliyun.com>
2024-04-03 11:42:59 +0800

    debug the upf parse

--- apns/module_io/citation.py
+++ apns/module_io/citation.py
@@ -1,40 +1,86 @@
+"""citation information got from Google Scholar, Web of Science, etc."""
+
 def citation(software: str = "abacus",
              pseudopot_nao: dict = None):
     
-    if pseudopot_nao is None:
-        pseudopot_nao = {}
-    # software: ABACUS, qespresso
-    if software == "abacus":
-        abacus()
-    elif software == "qespresso":
-        qespresso()
-    # python packages
-    seekpath()
-    materials_project()
-    
+    result = "\n"
+    result += "="*100
+    result += "START OF CITATION INFORMATION\n"
+    result += sssp()
+    result += qespresso() if software == "qespresso" else abacus()
+    result += seekpath()
+    result += materials_project()
+    result += "END OF CITATION INFORMATION\n"
+    result += "="*100
+    result += "\n"
+    print(result)
+
+def sssp():
+
+    result = "-"*100
+    result += "\n"
+    result += """
+We recommend user to read works done included in Standard Solid State Pseudopotentials (SSSP) library, which should be cited as:
+Prandini G, Marrazzo A, Castelli I E, et al. Precision and efficiency in solid-state pseudopotential calculations[J]. npj Computational Materials, 2018, 4(1): 72.
+Bosoni E, Beal L, Bercx M, et al. How to verify the precision of density-functional-theory implementations via reproducible and universal workflows[J]. Nature Reviews Physics, 2024, 6(1): 45-58.
+"""
+    result += "-"*100
+    result += "\n"
+    return result
 
 def seekpath():
 
-    print("The crystal symmetry analysis is done by py-seekpath, which is cited as:")
-    print("- Y. Hinuma, et al., Band structure diagram paths based on crystallography, Comp. Mat. Sci. 128, 140 (2017)")
-    print("SPGLIB is the kernel of symmetry analysis, which is cited as:")
-    print("- A. Togo, et al., Spglib: a software library for crystal symmetry search, arXiv:1808.01590 (2018)")
+    result = "-"*100
+    result += "\n"
+    result += """
+The crystal symmetry analysis is done by py-seekpath, which should be cited as:
+Hinuma Y, Pizzi G, Kumagai Y, et al. Band structure diagram paths based on crystallography[J]. Computational Materials Science, 2017, 128: 140-184.
+
+SPGLIB is the kernel of symmetry analysis, which should be cited as:
+Togo A, Tanaka I. Spglib: a software library for crystal symmetry search[J]. arXiv preprint arXiv:1808.01590, 2018.
+"""
+    result += "-"*100
+    result += "\n"
+    return result
 
 def materials_project():
 
-    print("The materials properties are obtained from Materials Project, which is cited as:")
-    print("- A. Jain, et al., A high-throughput infrastructure for density functional theory calculations, Comp. Mat. Sci. 50, 2295 (2011)")
+    result = "-"*100
+    result += "\n"
+    result += """
+The materials properties are obtained from Materials Project, which should be cited as:
+Jain A, Ong S P, Hautier G, et al. Commentary: The Materials Project: A materials genome approach to accelerating materials innovation[J]. APL materials, 2013, 1(1).
+"""
+    result += "-"*100
+    result += "\n"
+    return result
 
 def abacus():
     
-    print("The calculation is done by ABACUS, which is cited as:")
-    print("- C. Mohan, et al., Systematically improvable optimized atomic basis sets for ab initio calculations. Journal of Physics: Condensed Matter 22.44 (2010): 445501.")
-    print("- L. Pengfei, et al., Large-scale ab initio simulations based on systematically improvable atomic basis. Computational Materials Science 112 (2016): 503-517.")
+    result = "-"*100
+    result += "\n"
+    result += """
+The calculation is done by ABACUS, which should be cited as:
+Chen M, Guo G C, He L. Systematically improvable optimized atomic basis sets for ab initio calculations[J]. Journal of Physics: Condensed Matter, 2010, 22(44): 445501.
+Li P, Liu X, Chen M, et al. Large-scale ab initio simulations based on systematically improvable atomic basis[J]. Computational Materials Science, 2016, 112: 503-517.
+"""
+    result += "-"*100
+    result += "\n"
+    return result
 
 def qespresso():
 
-    print("The calculation is done by Quantum ESPRESSO, which is cited as:")
-    print("- P. Giannozzi, et al., QUANTUM ESPRESSO: a modular and open-source software project for quantum simulations of materials, J. Phys.: Condens. Matter 21, 395502 (2009)")
+    result = "-"*100
+    result += "\n"
+    result += """
+The calculation is done by Quantum ESPRESSO, which should be cited as:
+Giannozzi P, Baroni S, Bonini N, et al. QUANTUM ESPRESSO: a modular and open-source software project for quantum simulations of materials[J]. Journal of physics: Condensed matter, 2009, 21(39): 395502.
+Giannozzi P, Andreussi O, Brumme T, et al. Advanced capabilities for materials modelling with Quantum ESPRESSO[J]. Journal of physics: Condensed matter, 2017, 29(46): 465901.
+Giannozzi P, Baseggio O, Bonfà P, et al. Quantum ESPRESSO toward the exascale[J]. The Journal of chemical physics, 2020, 152(15).
+"""
+    result += "-"*100
+    result += "\n"
+    return result
     
 def pseudopotential(kind: list):
 
